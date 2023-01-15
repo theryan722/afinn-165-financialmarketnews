00f582f3a1b585d70d926a2d6d6ad00412af0e51
Ryan O'Day <romanguy722@gmail.com>
2023-01-15 14:12:21 -0500

    Financial modifications and additions

--- test.js
+++ test.js
@@ -1,10 +1,10 @@
 import assert from 'node:assert/strict'
 import test from 'node:test'
-import {afinn165} from './index.js'
+import {afinn165FinancialMarketNews} from './index.js'
 
-test('afinn', () => {
-  assert.equal(afinn165.positive, 2)
-  assert.equal(afinn165['self-deluded'], -2)
-  assert.equal(afinn165.damn, -2)
-  assert.equal(afinn165.futile, -2)
+test('afinn165FinancialMarketNews', () => {
+  assert.equal(afinn165FinancialMarketNews.positive, 2)
+  assert.equal(afinn165FinancialMarketNews['self-deluded'], -2)
+  assert.equal(afinn165FinancialMarketNews.damn, -2)
+  assert.equal(afinn165FinancialMarketNews.futile, -2)
 })
